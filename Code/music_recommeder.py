eb9176581ba472f66cb2fc4b8b686a3284b8c008
Rishabh Manoj <Rishabh.Manoj@iiitb.org>
2018-09-27 14:02:46 +0530

    Storing downloaded media(img and mp3)

--- Code/music_recommeder.py
+++ Code/music_recommeder.py
@@ -1,4 +1,6 @@
 import pickle
+import requests
+import os, errno
 
 PKL_MODEL = '../models/mood_model.pkl'
 
@@ -7,6 +9,19 @@ Moods = ['Peaceful','Upbeat','Happy','Tender','Romantic','Sensual','Energizing',
 #A dictionary of mood and track_details, (a tuple containing name of track, list of artists, preview_url, image_url and popularity) which is already sorted by popularity
 map_tracks_to_mood = pickle.load(open(PKL_MODEL,'rb'))
 
+def make_dirs(directory):
+	try:
+		os.makedirs(directory)
+	except OSError as e:
+		if e.errno != errno.EEXIST:
+			raise
+
+def store_media(url,name,MOOD_RES_FOLDER):
+	resp = requests.get(url,stream=True)
+	file_type = resp.headers['Content-Type'].split('/')[-1]
+	if(resp.ok):		
+		with open(os.path.join(MOOD_RES_FOLDER,name+'.'+file_type),'wb') as fin:
+			fin.write(resp.content)	
 
 def recommender(mood,n=5):
 	track_details = map_tracks_to_mood[mood]
@@ -34,8 +49,23 @@ def recommender(mood,n=5):
 			ind+=1	
 	return recommended_tracks
 
-#Testing	
+#Testing
+RESULT_FOLDER = 'results'	
 for mood in Moods:
-	val = recommender(mood,10)
-			
-		 
+	results = recommender(mood,100)
+
+	MOOD_RES_FOLDER = os.path.join(RESULT_FOLDER,mood)
+	make_dirs(MOOD_RES_FOLDER)
+	
+	for tracks in results:
+		name = tracks[0]
+		img_url = tracks[3]
+		mp3_url = tracks[2]
+		if(img_url):
+			store_media(img_url,name,MOOD_RES_FOLDER)
+		if(mp3_url):	
+			store_media(mp3_url,name,MOOD_RES_FOLDER)
+		
+		
+					
+					 
